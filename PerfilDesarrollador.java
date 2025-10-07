036fe361253e3bc9c6990ec4f57372ec2593d6d8
Julxs <60278994+Julxs@users.noreply.github.com>
2025-10-06 23:37:48 -0600

    Add files via upload

---
+++ PerfilDesarrollador.java
@@ -0,0 +1,14 @@
+public class PerfilDesarrollador {
+    public static void main(String[] args) {
+        System.out.println("nombre: Smook");
+        System.out.println("meta: convertirme en un experto en programacion en java y python");
+        System.out.println("Ciudad: Ciudad de mexico");
+        System.out.println("Motiviacion: Me gusta la teconologia");
+        System.out.println("Comida favorita: enchiladas verdes");
+        System.out.println("Color favorito: morado");
+
+        System.out.println("--------------------------------------------------------------------------");
+
+
+    }
+}
